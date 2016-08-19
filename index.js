1c52633913e4a844930bdf74df1bd865a680f233
Dor Shay <dor@poob.me>
2016-08-19 08:49:31 +0000

    Change API and add readme

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 var sql = require('mysql');
 var fs = require('fs');
+var pools = {};
+var globalPool = {};
 
 var model = {
-    config : function (configData,sqlPath)
+    addServer : function (configData,sqlPath,isGlobal)
     {
         configData.multipleStatements =  true
-        this.pool = sql.createPool(configData);
-        this.sqlPath = sqlPath.toString() + '/'
+        var server = {
+            pool : sql.createPool(configData),
+            sqlPath : sqlPath.toString() + '/',
+            fileQuery : function(path,data,cb){
+                if(cb)
+                    this.doQuery(path,[],cb)
+                else
+                    this.doQuery(path,data,cb)
+            },
+            doQuery : function(path,data,cb){
+                var that = this;
+                var filePath = path.charAt(0)!='%' ? this.sqlPath + path : path;
+                
+                fs.readFile(filePath,"utf-8",function(err,sqls)
+                {
+                    if(err) return cb(err)
+                    that.pool.query(sqls.toString(),data,cb)
+                })
+            },
+            query : function(x,y,z){
+                this.pool.query(x,y,z);
+            }
+        };
+        
+        var conName = configData.Name || configData.host.replace(/\./g,'_') + '_' + configData.database + '_' + configData.user
+        pools[conName] = server;
+        
+        if(isGlobal)
+            globalPool = server;
     },
-    query : function(path,data,cb)
+    getSevrer : function (serverName)
     {
-        if(cb)
-            this.doQuery(path,[],cb)
-        else
-            this.doQuery(path,data,cb)
+        var selectedPool = pools[serverName] || globalPool
+        
+        if(!selectedPool)
+            throw {error:'No such connection'}
+        
+        this.selectedPool = selectedPool;
     },
-    doQuery : function(path,data,cb)
+    getGlobalSevrer : function ()
     {
-        var that = this;
-        fs.readFile(this.sqlPath + path,"utf-8",function(err,sqls)
-        {
-            if(err) return cb(err)
-            that.pool.query(sqls.toString(),data,cb)
-        })
+        var selectedPool = globalPool
+        
+        if(!selectedPool)
+            throw {error:'No such connection'}
+        
+        this.selectedPool = selectedPool;
     }
 }
 module.exports = model;
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Shailta.JS
+## Node.js warper for mysql Coection managing and singale toning.
+### By Dor Shay (Poob)
+
+## Initalize:
+```
+var config = {
+    "connectionLimit" : 5,
+    "host" : "127.0.0.1",
+	"user" : "root",
+	"password" : "pwd",
+	"database" : "blabla"
+};
+
+var shaeilta = require("shaeiltajs");
+var psql = shaeilta.addServer(config.sql,__dirname + '/sql',true)
+```
+
+## Unsing:
+```
+psql.fileQuery('dashbord/load.sql',function(err,data)
+{
+    if(err) throw err
+    res.json(data)
+})
+
+psql.query('select 1 as test',function(err,data)
+{
+    if(err) throw err
+    res.json(data)
+})
+```
+
+## Using pool fro another file
+* Need to add server naming to reamme
+```
+var server = shaeilta.getSevrer('serverName')
+// Or get server defiend as global
+var server = shaeilta.getGlobalSevrer()
+```
